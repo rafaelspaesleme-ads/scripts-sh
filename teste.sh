adc5c508c967a27714105edf744471afb95edaf8
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2019-03-08 15:32:22 -0300

    up

--- teste.sh
+++ teste.sh
@@ -9,3 +9,5 @@ read host_ip_jenkins
 
 # Abrindo url do jenkins
 xdg-open http://$host_ip_jenkins:8080
+
+docker network create -d macvlan --subnet=192.168.0.0/24 --gateway=192.168.0.1  -o parent=eth0 pub_net
