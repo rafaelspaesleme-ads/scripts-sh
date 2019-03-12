45f8bdedf1995cddb25adfafdf31b98ebdf05c9a
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2019-03-12 18:24:27 -0300

    update install-docker-server-full

--- install-docker-env-dev-server-full.sh
+++ install-docker-env-dev-server-full.sh
@@ -5,8 +5,37 @@
 # Executado num ambiente Debian 8 com 2048 MB de memória;
 # Create per Rafael Paes Leme
 
+echo '+========================================================+'
+echo '=== = =                                            = = ==='
+echo '======== == -   BEM VINDO AO INSTALADOR DE   - == ========'
+echo '======== == - AUTOMATIZAÇÃO  DE  AMBIENTE DE - == ========'
+echo '======== == -   DESENVOLVIMENTO EM SERVIDOR  - == ========'
+echo '======== == -   --------------------------   - == ========'
+echo '======== == -              LINUX             - == ========'
+echo '======== == -            VERSÃO 1.0          - == ========'
+echo '=== = =                                            = = ==='
+echo '+========================================================+'
+echo '=== = =                                            = = ==='
+echo '======== == - RECOMENDAÇÕES:                 - == ========'
+echo '== == - Windows: Via PowerShell ou Git-bash  - == ========'
+echo '== == - Download Git-bash for Windows:       - == ========'
+echo '== == - Link: https://gitforwindows.org/     - == ========'
+echo '=== = =                                            = = ==='
+echo '+========================================================+'
+echo '=== = =                                            = = ==='
+echo '== == - Linux: Terminal local                - == ========'
+echo '=== = =                                            = = ==='
+echo '+========================================================+'
+echo '=== = =                                            = = ==='
+echo '== == - Tenha um otimo trabalho!!!!          - == ========'
+echo '=== = =                                            = = ==='
+echo '+========================================================+'
+
 # Instalando curl
 apt install curl
+
+# Fazendo conexão com o servidor via ssh
+curl -sSL https://github.com/rafaelspaesleme-ads/scripts-sh/raw/master/remote-access-shh.sh | sh
 
 # Configurando rede do servidor
 curl -sSL https://github.com/rafaelspaesleme-ads/scripts-sh/raw/master/config-network-server.sh | sh
