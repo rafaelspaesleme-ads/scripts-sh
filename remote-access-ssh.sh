230bd639726e179b7ecce2edf0311a29303416e2
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2019-03-13 09:23:29 -0300

    modify config script remote access.

--- remote-access-ssh.sh
+++ remote-access-ssh.sh
@@ -4,21 +4,11 @@
 # Execute esse script como root
 # Executado num ambiente Debian 8 com 2048 MB de memória;
 # Create per Rafael Paes Leme
-
 echo '+========================================================+'
 echo '=== = =                                            = = ==='
 echo '           BEM VINDO AO ACESSO REMOTO VIA SSH             '
 echo '=== = =                                            = = ==='
 echo '+========================================================+'
-
-# configurando ssh_config
-wget https://github.com/rafaelspaesleme-ads/scripts-sh/raw/master/ssh_config
-
-chmod 777 /etc/ssh/ssh_config
-
-mv -f ssh_config /etc/ssh/
-
-apt install sshpass
 
 # Realizando conexão com ambiente remoto via ssh
 echo 'Insira o usuário do ambiente remoto: '
@@ -29,6 +19,16 @@ read ip_remote
 
 echo 'Insira a senha do ambiente remoto: '
 read pass_remote
+
+# Instalando o controlador de password
+apt install sshpass
+
+# configurando ssh_config
+wget https://github.com/rafaelspaesleme-ads/scripts-sh/raw/master/ssh_config
+
+chmod 777 /etc/ssh/ssh_config
+
+mv -f ssh_config /etc/ssh/
 
 # Realizando acesso via ssh
 sshpass -p "$pass_remote" ssh $user_remote@$ip_remote
