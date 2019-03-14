db4f48ab7ed85299a21149d2d0c659d2234de065
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2019-03-14 18:03:55 -0300

    up

--- instalador_env/remote-access-ssh.sh
+++ instalador_env/remote-access-ssh.sh
@@ -10,50 +10,81 @@ echo '           BEM VINDO AO ACESSO REMOTO VIA SSH             '
 echo '=== = =                                            = = ==='
 echo '+========================================================+'
 
-# Realizando conexão com ambiente remoto via ssh
-echo 'Insira o usuário do ambiente remoto: '
-read user_remote
+# Iniciando configuração de rede.
+echo 'Qual seu sistema operacional?'
+echo 'Digite (w) para Windows ou (l) para linux: '
+read sisoperation
 
-echo 'Insira o ip do ambiente remoto: '
-read ip_remote
+    if [[ $sisoperation == "l" || $sisoperation == "L" ]]; then
 
-echo 'Insira a senha do ambiente remoto: '
-read pass_remote
+        # Realizando conexão com ambiente remoto via ssh
+        echo 'Insira o usuário do ambiente remoto: '
+        read user_remote_linux
 
-# Instalando scp
-apt-get install scp
+        echo 'Insira o ip do ambiente remoto: '
+        read ip_remote_linux
 
-# Instalando ssh
-apt-get install ssh
+        echo 'Insira a senha do ambiente remoto: '
+        read pass_remote
 
-# Instalando o controlador de password
-apt install sshpass
+        # Instalando scp
+        apt-get install scp
 
-# configurando ssh_config
-wget https://github.com/rafaelspaesleme-ads/scripts-sh/raw/master/instalador_env/seg/ssh_config
+        # Instalando ssh
+        apt-get install ssh
 
-chmod 777 /etc/ssh/ssh_config
+        # Instalando o controlador de password
+        apt install sshpass
 
-mv -f ssh_config /etc/ssh/
+        # configurando ssh_config
+        wget https://github.com/rafaelspaesleme-ads/scripts-sh/raw/master/instalador_env/seg/ssh_config
 
-# Instalando wget
-apt-get install wget
+        chmod 777 /etc/ssh/ssh_config
 
-# Instalando curl
-apt-get install curl
+        mv -f ssh_config /etc/ssh/
 
-# Instalando git
-apt-get install git
+        # Instalando wget
+        apt-get install wget
 
-# Baixando scripts sh
-git clone https://github.com/rafaelspaesleme-ads/scripts-sh.git
-chmod -R 777 scripts-sh
+        # Instalando curl
+        apt-get install curl
 
-# transferencia de dados local para servidor
-sshpass -p "$pass_remote" scp -r scripts-sh/instalador_env/install-docker-env-dev-server-full.sh $user_remote@$ip_remote: 
-sshpass -p "$pass_remote" scp -r scripts-sh/ $user_remote@$ip_remote:/tmp 
+        # Instalando git
+        apt-get install git
 
-rm -R scripts-sh/
+        # Baixando scripts sh
+        git clone https://github.com/rafaelspaesleme-ads/scripts-sh.git
+        chmod -R 777 scripts-sh
 
-# executando comando via ssh echo -e "$1\n" | sudo su -S
-sshpass -p "$pass_remote" ssh $user_remote@$ip_remote
+        # transferencia de dados local para servidor
+        sshpass -p "$pass_remote" scp -r scripts-sh/instalador_env/install-docker-env-dev-server-full.sh $user_remote_linux@$ip_remote_linux: 
+        sshpass -p "$pass_remote" scp -r scripts-sh/ $user_remote_linux@$ip_remote_linux:/tmp 
+
+        rm -R scripts-sh/
+
+        # executando comando via ssh echo -e "$1\n" | sudo su -S
+        sshpass -p "$pass_remote" ssh $user_remote@$ip_remote
+
+    fi
+
+    if [[ $sisoperation == "w" || $sisoperation == "W" ]]; then
+
+        # Realizando conexão com ambiente remoto via ssh
+        echo 'Insira o usuário do ambiente remoto: '
+        read user_remote_win
+
+        echo 'Insira o ip do ambiente remoto: '
+        read ip_remote_win
+
+        # Baixando scripts sh
+        git clone https://github.com/rafaelspaesleme-ads/scripts-sh.git
+        chmod -R 777 scripts-sh
+
+        # transferencia de dados local para servidor
+        scp -r scripts-sh/instalador_env/install-docker-env-dev-server-full.sh $user_remote_win@$ip_remote_win: 
+        scp -r scripts-sh/ $user_remote_win@$ip_remote_win:/tmp 
+
+        # executando comando via ssh echo -e "$1\n" | sudo su -S
+        ssh $user_remote_win@$ip_remote_win
+
+    fi
