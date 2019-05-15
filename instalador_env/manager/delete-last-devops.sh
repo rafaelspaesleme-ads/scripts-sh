5b62e0e6f9c452f5395355440d1b9f225386fc01
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-15 00:58:51 -0300

    Update and rename delete-last-env-devops.sh to delete-last-devops.sh

--- instalador_env/manager/delete-last-devops.sh
+++ instalador_env/manager/delete-last-devops.sh
@@ -2,8 +2,8 @@
 
 if [ -e "./DevOps" ]
    then
-      echo "O diretorio DevOps ja existe"
-   else
       echo "Excluindo repositorio antigo DevOps"
       rm -R ./DevOps/
+   else
+      echo "O diretorio DevOps nao existe"
 fi
