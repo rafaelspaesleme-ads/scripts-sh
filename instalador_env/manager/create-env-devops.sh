4658e8e6e8e66e61fcb254a2c8e9fad98d80ed1e
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-16 02:23:22 -0300

    Update create-env-devops.sh

--- instalador_env/manager/create-env-devops.sh
+++ instalador_env/manager/create-env-devops.sh
@@ -30,13 +30,3 @@ do
       mkdir "./DevOps/projects/repositories/${PROJECT_BREAK_DOWN[(($i))]}"
    fi
 done
-
-chmod -R 777 DevOps/
-
-# touch id_rsa.pub
-
-# chmod 777 id_rsa.pub
-
-# echo "${PUBLIC_KEY}" >> id_rsa.pub
-
-scp -i /var/jenkins_home/.ssh/$CREATE_PPKEY DevOps/ $USER_ENV@$HOST_ENV:/$USER
