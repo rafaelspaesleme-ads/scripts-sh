fb0dfcaa3d6be8d5b8825cb9356ef81885925434
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-16 01:23:36 -0300

    Update create-env-devops.sh

--- instalador_env/manager/create-env-devops.sh
+++ instalador_env/manager/create-env-devops.sh
@@ -39,4 +39,4 @@ chmod -R 777 DevOps/
 
 # echo "${PUBLIC_KEY}" >> id_rsa.pub
 
-scp -i ~/.ssh/$CREATE_PPKEY.pub DevOps/ $USER_ENV@$HOST_ENV:/$USER
+scp -i ~/.ssh/$CREATE_PPKEY DevOps/ $USER_ENV@$HOST_ENV:/$USER
