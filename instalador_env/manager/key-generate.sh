64dff31299bc8d5718a9339de69037b78de34525
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-16 01:30:24 -0300

    Update key-generate.sh

--- instalador_env/manager/key-generate.sh
+++ instalador_env/manager/key-generate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Gerando chave ssh
+cd /var/jenkins_home/.ssh
+
 ssh-keygen -t rsa -N "" -f $CREATE_PPKEY
 
-ssh-copy-id -i ~/.ssh/$CREATE_PPKEY.pub $USER_ENV@$HOST_ENV
+ssh-copy-id -i /var/jenkins_home/.ssh/$CREATE_PPKEY.pub $USER_ENV@$HOST_ENV
