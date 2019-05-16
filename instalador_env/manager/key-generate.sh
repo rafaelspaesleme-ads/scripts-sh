d4e75c0ff992a9f7126053113e7e02457922c50b
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-05-16 02:02:50 -0300

    Update key-generate.sh

--- instalador_env/manager/key-generate.sh
+++ instalador_env/manager/key-generate.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 # Gerando chave ssh
 cd ~/.ssh
 
 ssh-keygen -t rsa -N "" -f $CREATE_PPKEY
 
-sshpass -p "" ssh-copy-id -i ~/.ssh/$CREATE_PPKEY.pub $USER_ENV@$HOST_ENV
+ssh-copy-id -i ~/.ssh/$CREATE_PPKEY.pub $USER_ENV@$HOST_ENV
